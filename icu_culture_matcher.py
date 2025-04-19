f73c95a5b424d1da52275183d491c41dd8f99d8c
choi-doubley <cyypedr@gmail.com>
2025-04-19 20:38:39 +0900

    Update icu_culture_matcher.py

--- icu_culture_matcher.py
+++ icu_culture_matcher.py
@@ -61,6 +61,13 @@ def detect_delimiter(series):
 # Streamlit 시작
 st.set_page_config(page_title="NICU KONIS Matcher", layout="centered")
 st.markdown("<h1 style='text-align:center;'>👶 NICU KONIS<br>혈액배양양성환자 작성 도우미</h1>", unsafe_allow_html=True)
+st.markdown("""
+<div style='text-align:center; font-size: 0.9em; color: gray;'>
+최종 업데이트: 2025-04-19<br>
+문의: cyypedr@gmail.com
+</div>
+""", unsafe_allow_html=True)
+
 
 # 파일 업로드
 icu_file = st.file_uploader("👶 중환자실 입퇴실 파일", type=["xlsx"])
