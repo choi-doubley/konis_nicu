b4c6e89fcf5947280eeb1ed3f1eaf214d3bfc305
choi-doubley <cyypedr@gmail.com>
2025-04-21 16:56:26 +0900

    Update icu_culture_matcher.py

--- icu_culture_matcher.py
+++ icu_culture_matcher.py
@@ -242,7 +242,12 @@ if icu_file and culture_file:
             except Exception as e:
                 st.warning(f"⚠️ 생년월일 병합에 실패했습니다: {e}")
 
-       
+        # 병동(시행부서)도 병합 방식으로 가져오기
+        ward_df = culture_df[[culture_id, culture_date, culture_ward]].copy()
+        ward_df.rename(columns={culture_ward: "병동"}, inplace=True)
+        result = result.merge(ward_df, on=[culture_id, culture_date], how="left")
+
+        
         # 컬럼명 정리
         result.rename(columns={
             culture_id: "환자ID",
@@ -254,10 +259,6 @@ if icu_file and culture_file:
         if use_result_col:
             result.rename(columns={culture_result: "분리균"}, inplace=True)
 
-        # 병동(시행부서)도 병합 방식으로 가져오기
-        ward_df = culture_df[[culture_id, culture_date, culture_ward]].copy()
-        ward_df.rename(columns={culture_ward: "병동"}, inplace=True)
-        result = result.merge(ward_df, on=[culture_id, culture_date], how="left")
 
         # 비고 컬럼 추가: NICU/신생아 포함 + ICU 입실정보가 없는 경우
         result["비고"] = None
